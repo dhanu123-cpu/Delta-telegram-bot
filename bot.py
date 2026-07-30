0b60373933ef115354088763481e2f757732b48b
dhanu123-cpu <officialdhanu9975@gmail.com>
2026-07-30 18:35:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,8 +7,8 @@ from threading import Thread
 from flask import Flask
 app = Flask(__name__)
 # Telegram & Trading Configuration
-BOT_TOKEN = "8380158711:AAHDYOV81|JvsnQkMaQfa7yqkOVeOEFmpxo"
-CHAT_ID = "8380158711"
+BOT_TOKEN = "8380158711:AAG31VOUNc1L-Aw_qNAiOMoMUF2FQAQkvbo"
+CHAT_ID = "7755539827"
 TIMEFRAMES = ["1m", "3m", "5m", "15m", "30m", "60m"]
 SYMBOL = "BTCUSDT"
 @app.route('/')
