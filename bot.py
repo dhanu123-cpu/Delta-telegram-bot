f7433bf9a5bf87c9dc9f64bf5204955b23a4a078
dhanu123-cpu <officialdhanu9975@gmail.com>
2026-08-03 23:41:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -52,10 +52,8 @@ def check_strategy(tf):
     c3 = df.iloc[-3]
     c2 = df.iloc[-2]
     c1 = df.iloc[-1]
-
-    cross_up = (c3['ema9'] <= c3['ema20']) and (c2['ema9'] > c2['ema20'])
-    cross_down = (c3['ema9'] >= c3['ema20']) and (c2['ema9'] < c2['ema20'])
-
+cross_up = (c2['ema9'] < c2['ema20']) and (c1['ema9'] >= c1['ema20'])
+cross_down = (c2['ema9'] > c2['ema20']) and (c1['ema9'] <= c1['ema20'])
     if cross_up:
         msg = f"🚀 <b>DELTA BUY SIGNAL ({SYMBOL})</b>\n⏱️ Timeframe: <b>{tf}</b>\n🟢 EMA 9 Crossed Above EMA 20\n💰 Price: {c1['close']}"
         send_telegram_signal(msg)
